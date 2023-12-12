4900bdb327af663b352abfae81fac9b1138e40cc
Zlatko Cvetkovski <zlatko@unlimitedcoders.com>
2023-12-12 13:53:54 +0100

    Add custom-preset.js for additional tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,15 @@ const colors = require('./colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    presets: [
+        require('./custom-preset.js')
+    ],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
     theme: {
         extend: {
             fontFamily: {
